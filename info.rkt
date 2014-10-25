d6ec759c4ba418d3f02e4cc51ba4627b7633c62f
Ryan Davis <ryand-ruby@zenspider.com>
2014-10-24 17:09:42 -0700

    argh

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
-(define version "1.0")
+(define version "1.0.0")
 
 (define collection "check-sexp-equal")
 
-(define deps '("sexp-diff" :version "0.1"))
+(define deps '(("sexp-diff" #:version "0.1")))
